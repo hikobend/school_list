a1df9f5171bea62eeed7b63cec8d3c618b0929c0
hikobend <hiko.bkend@gmail.com>
2022-09-22 22:03:40 +0900

    Operatorテーブル作成

---
+++ app/models/base.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"crypto/sha1"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"example.com/school/config"
+	// ドライバのインストール
+	"github.com/google/uuid"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var Db *sql.DB
+
+// エラーの宣言
+var err error
+
+// テーブル名の宣言
+const (
+	tableNameOperator = "operators"
+)
+
+// テーブルはmain関数の前に作成
+func init() {
+	// データーベースとエラー。ドライバとデーターベース名
+	Db, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
+	// エラーハンドリング
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmdO := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		uuid STRING NOT NULL UNIQUE,
+		name STRING,
+		email STRING,
+		password STRING,
+		created_at DATETIME)`, tableNameOperator)
+
+	Db.Exec(cmdO)
+}
+
+// UUID作成
+// 返り値をuuidobj
+func createUUID() (uuidobj uuid.UUID) {
+	// NewUUIDを使用
+	uuidobj, _ = uuid.NewUUID()
+	// returnで返す
+	return uuidobj
+}
+
+// パスワード作成
+func Encrypt(plaintext string) (cryptext string) {
+	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	// returnで返す
+	return cryptext
+}
