83ff80d9217328cc89552a20bfd5aaa2d6345507
hikobend <hiko.bkend@gmail.com>
2022-09-23 18:56:27 +0900

    get schoolsByOperator確認

--- main.go
+++ main.go
@@ -9,10 +9,19 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	operator, _ := models.GetOperator(2)
-	operator.CreateSchool("testtest")
+	o := &models.Operator{}
+	o.Name = "test3"
+	o.Email = "test3@test3.com"
+	o.Name = "test3test"
+	fmt.Println(o)
 
-	schools, _ := models.GetSchools()
+	o.CreateOperator()
+
+	operator, _ := models.GetOperator(3)
+	operator.CreateSchool("third school")
+
+	operator2, _ := models.GetOperator(2)
+	schools, _ := operator2.GetSchoolByOperator()
 	for _, v := range schools {
 		fmt.Println(v)
 	}
