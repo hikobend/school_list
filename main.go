93750fb72342d1d2eaf8ad435a8ec6b2e4b9a750
hikobend <hiko.bkend@gmail.com>
2022-09-23 20:55:19 +0900

    server起動

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
+	"example.com/school/app/controllers"
 	"example.com/school/app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
+	controllers.StartmainServer()
 }
