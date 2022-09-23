261f3fa0f6f7750e3b68af1e79df686c8311a43a
hikobend <hiko.bkend@gmail.com>
2022-09-23 19:11:59 +0900

    update school確認

--- main.go
+++ main.go
@@ -9,20 +9,7 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	o := &models.Operator{}
-	o.Name = "test3"
-	o.Email = "test3@test3.com"
-	o.Name = "test3test"
-	fmt.Println(o)
-
-	o.CreateOperator()
-
-	operator, _ := models.GetOperator(3)
-	operator.CreateSchool("third school")
-
-	operator2, _ := models.GetOperator(2)
-	schools, _ := operator2.GetSchoolByOperator()
-	for _, v := range schools {
-		fmt.Println(v)
-	}
+	s, _ := models.GetSchool(2)
+	s.Name = "second school"
+	s.UpdateSchool()
 }
