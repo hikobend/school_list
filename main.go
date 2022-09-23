7ffb7bca6be650c7cfa90c1a685eedacc6081356
hikobend <hiko.bkend@gmail.com>
2022-09-23 10:50:03 +0900

    不要文削除

--- main.go
+++ main.go
@@ -9,15 +9,4 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	o, _ := models.GetOperator(1)
-
-	o.Name = "hello"
-	o.Email = "hello@hello.com"
-	o.UpdateOperator()
-	o, _ = models.GetOperator(1)
-	fmt.Println(o)
-
-	o.DeleteOperator()
-	o, _ = models.GetOperator(1)
-	fmt.Println(o)
 }
