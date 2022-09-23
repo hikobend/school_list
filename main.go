a4620f00835a72d03a34f84cc90cbb1aa513f4a9
hikobend <hiko.bkend@gmail.com>
2022-09-23 09:02:47 +0900

    operator create確認

--- main.go
+++ main.go
@@ -10,5 +10,10 @@ func main() {
 	fmt.Println(models.Db)
 
 	o, _ := models.GetOperator(1)
+
+	o.Name = "hello"
+	o.Email = "hello@hello.com"
+	o.UpdateOperator()
+	o, _ = models.GetOperator(1)
 	fmt.Println(o)
 }
