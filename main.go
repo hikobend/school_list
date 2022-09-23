5c9811567b1df3014abdce5a51573da6df94968a
hikobend <hiko.bkend@gmail.com>
2022-09-23 16:58:23 +0900

    school create確認

--- main.go
+++ main.go
@@ -9,4 +9,15 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
+	o := models.Operator{}
+	o.Name = "test"
+	o.Email = "test@test.com"
+	o.PasaWord = "test@test.com"
+
+	fmt.Println(o)
+
+	o.CreateOperator()
+
+	operator, _ := models.GetOperator(2)
+	operator.CreateSchool("name1")
 }
