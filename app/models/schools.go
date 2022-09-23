e89ea9401fe279773362d8c5c43128a0d06e6c94
hikobend <hiko.bkend@gmail.com>
2022-09-23 17:50:00 +0900

    get schools

--- app/models/schools.go
+++ app/models/schools.go
@@ -34,7 +34,8 @@ func GetSchool(id int) (school School, err error) {
 
 	school = School{}
 
-	err = Db.QueryRow(cmd, id).Scan(&school.ID,
+	err = Db.QueryRow(cmd, id).Scan(
+		&school.ID,
 		&school.Name,
 		&school.OperatorID,
 		&school.CreatedAt)
@@ -44,4 +45,32 @@ func GetSchool(id int) (school School, err error) {
 	}
 
 	return school, err
+}
+
+func GetSchools() (schools []School, err error) {
+	cmd := `select id, name, operator_id, created_at from schools`
+	rows, err := Db.Query(cmd)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// 取り出す
+	for rows.Next() {
+		var school School
+		err = rows.Scan(
+			&school.ID,
+			&school.Name,
+			&school.OperatorID,
+			&school.CreatedAt)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		schools = append(schools, school)
+	}
+	rows.Close()
+
+	return schools, err
 }
