5ff0af35b7e7bf17f5ab16fbeda2f38226d02480
hikobend <hiko.bkend@gmail.com>
2022-09-24 14:16:22 +0900

    反映検証

--- main.go
+++ main.go
@@ -2,24 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 
+	"example.com/school/app/controllers"
 	"example.com/school/app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
-	// controllers.StartmainServer()
+	controllers.StartmainServer()
 
-	operator, _ := models.GetOperatorByEmail("email@email.com")
-	fmt.Println(operator)
-
-	session, err := operator.CreateSession()
-	if err != nil {
-		log.Panicln(err)
-	}
-	fmt.Println(session)
-
-	valid, _ := session.CheckSession()
-	fmt.Println(valid)
 }
