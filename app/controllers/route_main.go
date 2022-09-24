a3782cd26e15a4d7f7c8aa676db7ba8deae192ef
hikobend <hiko.bkend@gmail.com>
2022-09-24 18:11:07 +0900

    new, create追加

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -28,3 +28,34 @@ func index(w http.ResponseWriter, r *http.Request) {
 		generateHTML(w, operator, "layout", "private_navbar", "index")
 	}
 }
+
+func schoolNew(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "school_new")
+	}
+}
+
+func schoolSave(w http.ResponseWriter, r *http.Request) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err = r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		operator, err := sess.GetOperatorBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		name := r.PostFormValue("name")
+		if err := operator.CreateSchool(name); err != nil {
+			log.Println(err)
+		}
+
+		http.Redirect(w, r, "/schools", 302)
+	}
+}
