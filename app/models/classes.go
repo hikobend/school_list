3d971b293cd687f968871bce0d2a4e9759cd9c2e
hikobend <hiko.bkend@gmail.com>
2022-09-25 11:54:42 +0900

    CreateClass作成

--- app/models/classes.go
+++ app/models/classes.go
@@ -1,10 +1,30 @@
 package models
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 type Class struct {
 	ID          int
 	ClassNumber string
 	SchoolID    int
 	CreatedAt   time.Time
+}
+
+func (s *School) CreateClass(class_number string) (err error) {
+	cmd := `insert into classes(
+		class_number, 
+		school_id, 
+		created_at) values (?, ?, ?)`
+
+	_, err = Db.Exec(cmd,
+		class_number,
+		s.ID,
+		time.Now())
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
 }
