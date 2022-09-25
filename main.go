e4d7f4500a8f7e3af53149b567e1dc9542819d3e
hikobend <hiko.bkend@gmail.com>
2022-09-25 13:08:45 +0900

    GetClassBySchool確認

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	fmt.Println(models.Db)
+	// school, _ := models.GetSchool(3)
+	// school.CreateClass("third class")
 
-	school, _ := models.GetSchool(2)
-	school.CreateClass("second class")
-
-	classes, _ := models.GetClasses()
+	school2, _ := models.GetSchool(1)
+	classes, _ := school2.GetClassBySchool()
 	for _, v := range classes {
 		fmt.Println(v)
 	}
