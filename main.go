2a612ef8f299f5d7240e7a202a82c1b22b56c6d6
hikobend <hiko.bkend@gmail.com>
2022-09-25 11:56:01 +0900

    CreateClass確認

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 
-	"example.com/school/app/controllers"
 	"example.com/school/app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
-	controllers.StartmainServer()
 
+	o := &models.Operator{}
+	o.Name = "operator1"
+	o.Email = "email@email.com"
+	o.PassWord = "password"
+
+	fmt.Println(o)
+
+	o.CreateOperator()
+
+	operator, _ := models.GetOperator(1)
+	operator.CreateSchool("first school")
+
+	school, _ := models.GetSchool(1)
+	school.CreateClass("first class")
 }
