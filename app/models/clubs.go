5238976c85b11dd4794a474f69876677f0f678cd
hikobend <hiko.bkend@gmail.com>
2022-09-25 15:41:52 +0900

    CreateClub作成

--- app/models/clubs.go
+++ app/models/clubs.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 type Clob struct {
 	ID        int
@@ -8,4 +11,25 @@ type Clob struct {
 	Content   string
 	SchoolID  int
 	CreatedAt time.Time
+}
+
+func (s *School) CreateClub(name string, content string) (err error) {
+	cmd := `insert into clubs(
+		name,
+		content,
+		school_id,
+		created_at) values (?, ?, ?, ?)`
+
+	_, err = Db.Exec(cmd,
+		name,
+		content,
+		s.ID,
+		time.Now(),
+	)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return err
 }
